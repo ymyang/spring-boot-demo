e3c02391035d74d7208163fa652d81f5cfe06217
杨义明(集团控股) <yangym02@kaisagroup.com>
2017-07-22 15:46:14 +0800

    use druid

--- src/main/java/com/ymiyun/springboot/simple/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ymiyun.springboot.simple.repository;
-
-import com.ymiyun.springboot.simple.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long>{
-
-}
